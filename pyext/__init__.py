f780e9dede4290bf72702714fbedf3221a0be802
Take Vos <take.vos@vosgames.nl>
2012-09-02 12:16:53 +0200

    Service is now located in tvnet, remove references to it.

--- pyext/__init__.py
+++ pyext/__init__.py
@@ -6,11 +6,4 @@ c_tvutils.tvu_init(sys.argv[0])
 
 time = c_tvutils.tvu_time
 random = c_tvutils.tvu_random
-service_socket = c_tvutils.tvu_service_socket
-service_client_bind = c_tvutils.tvu_service_client_bind
-service_bind = c_tvutils.tvu_service_bind
-service_unbind = c_tvutils.tvu_service_unbind
-service_close = c_tvutils.tvu_service_close
-service_sendto = c_tvutils.tvu_service_sendto
-service_recvfrom = c_tvutils.tvu_service_recvfrom
 
