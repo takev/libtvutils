b00723fa529d11a62dc96a27ce91f915abf0af05
Take Vos <take.vos@vosgames.nl>
2012-09-02 15:19:07 +0200

    Adding an increment and wrap function, for walking through finite tables.

--- tvutils/macros.h
+++ tvutils/macros.h
@@ -17,10 +17,18 @@
 #ifndef TVU_MACROS_H
 #define TVU_MACROS_H
 
-#define TVU_MAX(a, b)   ((a) > (b) ? (a) : (b))
-#define TVU_MIN(a, b)   ((a) < (b) ? (a) : (b))
-#define TVU_UNSIGNED(a) ((a) > 0  ? (a) : 0)
-#define TVU_CLAMP(a)    ((a) > -1 ? ((a < 1) ? (a) : 1) : -1)
-#define TVU_ABS(a)      ((a) >= 0 ? (a) : -(a))
+#define TVU_MAX(a, b)       ((a) > (b) ? (a) : (b))
+#define TVU_MIN(a, b)       ((a) < (b) ? (a) : (b))
+#define TVU_UNSIGNED(a)     ((a) > 0  ? (a) : 0)
+#define TVU_CLAMP(a)        ((a) > -1 ? ((a < 1) ? (a) : 1) : -1)
+#define TVU_ABS(a)          ((a) >= 0 ? (a) : -(a))
+
+/** Increment value and wrap around.
+ * @param a     value to increment
+ * @param n     number of values, beyond value wraps to 0.
+ * @returns     incremented values, or wrapped to 0.
+ */
+#define TVU_INCWRAP(a, n)   (((a) + 1) < (n) ? ((a) + 1) : 0)
+
 
 #endif
